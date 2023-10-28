3a4df900445cc0368dd92056ef06f3c275c8fa6f
9raja1999 <draj29209@gmail.com>
2023-10-28 13:39:54 -0700

    naming convention corrected

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new HttpExceptionFilter)
+  // Bootstrap posrt 300 on listen
   await app.listen(3000);
 }
 bootstrap();
